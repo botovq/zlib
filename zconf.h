f86b109927555ebb0f90fc122d80132c63b75c67
Theo Buehler <tb@openbsd.org>
2022-08-15 14:06:27 +0200

    We have unistd.h and stdarg.h

--- zconf.h
+++ zconf.h
@@ -434,11 +434,11 @@ typedef uLong FAR uLongf;
    typedef unsigned long z_crc_t;
 #endif
 
-#ifdef HAVE_UNISTD_H    /* may be set to #if 1 by ./configure */
+#if 1
 #  define Z_HAVE_UNISTD_H
 #endif
 
-#ifdef HAVE_STDARG_H    /* may be set to #if 1 by ./configure */
+#if 1
 #  define Z_HAVE_STDARG_H
 #endif
 
